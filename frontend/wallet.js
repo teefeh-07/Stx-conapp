dbb53b4e69e14072ed3a87e76c14cca300ac8264
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-17 13:46:12 +0100

    feat: init wallet setup file

--- frontend/wallet.js
+++ frontend/wallet.js
@@ -1,6 +1,1 @@
 // Wallet Connect Implementation
-import { AppConfig, UserSession, showConnect } from "@stacks/connect";
-import { StacksMocknet } from "@stacks/network";
-
-const appConfig = new AppConfig(["store_write", "publish_data"]);
-export const userSession = new UserSession({ appConfig });
