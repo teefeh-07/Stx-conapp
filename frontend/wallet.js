7a09ebe3d6882cb0927e07f2d661c16dcb304327
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-17 13:46:14 +0100

    feat: create user session

--- frontend/wallet.js
+++ frontend/wallet.js
@@ -3,3 +3,4 @@ import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 import { StacksMocknet } from "@stacks/network";
 
 const appConfig = new AppConfig(["store_write", "publish_data"]);
+export const userSession = new UserSession({ appConfig });
