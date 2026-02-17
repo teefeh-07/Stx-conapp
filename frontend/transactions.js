252d4224477b49d06f58e98c5d003574f9e7671e
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-17 14:03:18 +0100

    config: set up network instance

--- frontend/transactions.js
+++ frontend/transactions.js
@@ -1,3 +1,5 @@
 // Stacks Transactions Implementation
 import { makeContractCall, broadcastTransaction, AnchorMode } from "@stacks/transactions";
 import { StacksMocknet } from "@stacks/network";
+
+const network = new StacksMocknet();
