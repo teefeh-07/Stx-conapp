0bc98141a5938606b8f8155fbc2c6cc5195ba1f4
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-17 14:03:33 +0100

    feat: implement subscription listener

--- frontend/chainhooks.js
+++ frontend/chainhooks.js
@@ -4,3 +4,4 @@ import { ChainhooksClient } from "@hirosystems/chainhooks-client";
 const client = new ChainhooksClient();
 
 export const subscribe = (topic) => {
+  client.subscribe(topic, (event) => {
