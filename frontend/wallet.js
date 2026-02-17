a5483ff281a75fd7dbbace08b3f6390fea804787
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-17 13:46:13 +0100

    feat: configure app config

--- frontend/wallet.js
+++ frontend/wallet.js
@@ -1,3 +1,5 @@
 // Wallet Connect Implementation
 import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 import { StacksMocknet } from "@stacks/network";
+
+const appConfig = new AppConfig(["store_write", "publish_data"]);
